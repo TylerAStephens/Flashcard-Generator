b2d564df8f873b3b78003ea48766bf455f85f928
TylerAStephens <tylerstephens16@yahoo.com>
2017-09-27 22:15:58 -0400

    Added makeCard function and added array to hold all new cards

--- BasicCard.js
+++ BasicCard.js
@@ -7,8 +7,20 @@ function BasicCard(front,back) {
     //Made print info function to console log front and back
     this.printInfo = function() {
         console.log("Front: " + this.front, "Back: " + this.back);
+        // added line to clean up node log
+        console.log("---------------");        
     };
 }
+// var to keep track of how many cards have been made
+var count = 0;
+// array to hold cards made
+var cardArray = [];
+
+// made a make card function to run the prompt and know when to stop
+var makeCards = function() {
+    // If statement to cap number of cards to 5
+    if (count < 5) {
+        console.log("New Card");
 // used inquirer prompt to ask the user for input to add to the cards
 inquirer.prompt([
     {
@@ -24,6 +36,23 @@ inquirer.prompt([
 ]).then(function(answers) {
     //create a variable to assign each new card with given answers
     var newCard = new BasicCard(answers.front, answers.back);
-    //calls printInfo function for the new flash card
-    newCard.printInfo();
+   //adds each new card to the cardArray holding all of the users input
+   cardArray.push(newCard);
+   //add to the count
+   count++;
+   //runs prompt again
+   makeCards();
 });
+// If all new cards are made
+}else{
+    // For loop for putting all objects added to array in line
+    for (var x = 0; x < cardArray.length; x++) {
+        // Print array info
+        cardArray[x].printInfo();
+    }
+}
+
+}; //ending make card function
+
+// Run make cards function
+makeCards();
